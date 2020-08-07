1f49a86e575a99f3d046aaa59d8cffd702f5da70
CELAL KUTLUER <57915975+celalkutluer@users.noreply.github.com>
2020-08-07 18:29:46 +0300

    Initial commit

---
+++ lib/Pages/LoginPage.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/Pages/HomePage.dart';
+import 'package:flutter_chat_app/Widgets/ProgressWidget.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class LoginScreen extends StatefulWidget {
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+
+}
